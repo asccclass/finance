a0d02b3f90cf4bf7aeabf7651fbf2be53bf723d4
Andy Liu <andyliu@gate.sinica.edu.tw>
2023-08-02 03:50:11 +0000

    <refector>success

--- static/js/liabilities.js
+++ static/js/liabilities.js
@@ -17,5 +17,5 @@ $.ajax({
    url: "https://www.justdrink.com.tw/finance/read/liabilities.json",
    method: 'GET',
    async: true,　//  預設是true=非同步,false=同步 (true時整行可省略)
-   success: drawTable(res)
+   success: function(res) { drawTable(res); }
 });
