b3b13348154ce1af302322273b21c2b9c7e05b16
Andy Liu <andyliu@gate.sinica.edu.tw>
2023-08-02 03:24:13 +0000

    <docs>將內容於表格中顯示

--- static/js/liabilities.js
+++ static/js/liabilities.js
@@ -1,3 +1,17 @@
+finction drawTable(res) {
+   res.forEach(order => {
+      const tr = document.createElement('tr');
+      const trContent = `
+          <td>${order.productName}</td>
+          <td>${order.productNumber}</td>
+          <td>${order.paymentStatus}</td>
+          <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
+          <td class="primary">Details</td>
+      `;
+      tr.innerHTML = trContent;
+      document.querySelector('table tbody').appendChild(tr);
+  });
+}
 
 $.ajax({
    url: "https://www.justdrink.com.tw/finance/read/liabilities.json",
@@ -5,7 +19,5 @@ $.ajax({
    async: true,　//  預設是true=非同步,false=同步 (true時整行可省略)
    data: {
    },
-   success: function(res) {
- console.log(res);
-   }
+   success: drawTable(res)
 });
