ec23f78a90a1c3e8c866645e6bbd1a068a4004a0
Andy Liu <andyliu@gate.sinica.edu.tw>
2023-08-02 03:34:23 +0000

    <docs>顯示表格

--- static/js/liabilities.js
+++ static/js/liabilities.js
@@ -2,11 +2,11 @@ finction drawTable(res) {
    res.forEach(order => {
       const tr = document.createElement('tr');
       const trContent = `
-          <td>${order.productName}</td>
-          <td>${order.productNumber}</td>
-          <td>${order.paymentStatus}</td>
-          <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
-          <td class="primary">Details</td>
+          <td>${order.bank}</td>
+          <td>${order.total}</td>
+          <td>${order.start}</td>
+          <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.year}</td>
+          <td class="primary">${order.period}</td>
       `;
       tr.innerHTML = trContent;
       document.querySelector('table tbody').appendChild(tr);
@@ -17,7 +17,5 @@ $.ajax({
    url: "https://www.justdrink.com.tw/finance/read/liabilities.json",
    method: 'GET',
    async: true,　//  預設是true=非同步,false=同步 (true時整行可省略)
-   data: {
-   },
    success: drawTable(res)
 });
